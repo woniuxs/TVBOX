62f69630526cd767a5e7ab856327e06054cc2d71
woniu <136662130+woniuxs@users.noreply.github.com>
2025-01-24 20:27:40 +0800

    Update sd.php

--- 4K/sd.php
+++ 4K/sd.php
@@ -5,13 +5,13 @@
       {
         "name": "微信赞助打赏",
         "url": " ",
-        "icon": "https://alicliimg.clewm.net/342/790/68790342/170832351526029731899cac9d8ca935cdc736eec31af1708323503.png",
+        "icon": "https://tv.xn--11x555b.top/TV/20241223002929.png",
         "version": "感谢支持"
       },
       {
         "name": "公众号蜗牛科技仓",
         "url": " ",
-        "icon": "https://alicliimg.clewm.net/342/790/68790342/17088361435038ca103a5d9bd4661a38f9d63262b39ba1708836138.png",
+        "icon": "https://tv.xn--11x555b.top/TV/1_1035341046_171_85_3_977367012_c21a382d7884adf3ef71d82eaddcfe0c.png",
         "version": ""
       }
     ]
@@ -20,13 +20,13 @@
     "name": "本地包",
     "list": [
       {
-        "name": "多线路半本地包01-24",
+        "name": "多线路半本地包",
         "url": "http://123.yy.xn--dkw.xn--6qq986b3xl/tvbox.zip",
         "icon": "http://127.0.0.1:9978/file/tvbox/.版本号.png",
         "version": "当前版本防止域名被屏蔽用"
       },
       {
-        "name": "潇洒线上包1.22.1",
+        "name": "本地线上包",
         "url": " https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737688390644/单线路.zip",
         "icon": "http://127.0.0.1:9978/file/TVBoxOSC/tvbox/.版本.png",
         "version": "当前版本自行更新"
@@ -37,13 +37,13 @@
     "name": "Allinone",
     "list": [
       {
-        "name": "线上包-32-1.23",
+        "name": "线上包-32位",
         "url": "http://wb.xn--dkw.xn--6qq986b3xl:5244/d/%E5%A4%A9/allinone32.zip",
         "icon": "http://127.0.0.1:9978/file/TVBox/.allinone.png",
         "version": "当前版本胖羊专用"
       },
       {
-        "name": "线上包-64-1.23",
+        "name": "线上包-64位",
         "url": "http://wb.xn--dkw.xn--6qq986b3xl:5244/d/%E5%A4%A9/allinone64.zip",
         "icon": "http://127.0.0.1:9978/file/TVBox/.allinone.png",
         "version": "当前版本胖羊专用"
