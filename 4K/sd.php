3873f207d1a041cd397e124c92fe463706cfc8fb
woniu <136662130+woniuxs@users.noreply.github.com>
2025-01-24 19:03:40 +0800

    Update sd.php

--- 4K/sd.php
+++ 4K/sd.php
@@ -30,24 +30,6 @@
         "url": " https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737688390644/单线路.zip",
         "icon": "http://127.0.0.1:9978/file/TVBoxOSC/tvbox/.版本.png",
         "version": "当前版本自行更新"
-      },
-      {
-        "name": "欧歌助手",
-        "url": "http://wb.xn--dkw.xn--6qq986b3xl:5244/d/%E5%A4%A9/%E6%AC%A7%E6%AD%8C%E5%8A%A9%E6%89%8B.apk",
-        "icon": "https://api.dzzui.com/api/avatar",
-        "version": "手机版"
-      },
-      {
-        "name": "微信赞助打赏",
-        "url": "http://wb.nxog.top:5244/d/移/软件/0.png",
-        "icon": "https://alicliimg.clewm.net/342/790/68790342/170832351526029731899cac9d8ca935cdc736eec31af1708323503.png",
-        "version": "感谢支持"
-      },
-      {
-        "name": "公众号欧歌APP",
-        "url": "http://wb.nxog.top:5244/d/移/软件/wx.png",
-        "icon": "https://alicliimg.clewm.net/342/790/68790342/17088361435038ca103a5d9bd4661a38f9d63262b39ba1708836138.png",
-        "version": ""
       }
     ]
   },
