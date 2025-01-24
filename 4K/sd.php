48446292b860ae47c5b8fc05e4a2c62c6021845d
woniu <136662130+woniuxs@users.noreply.github.com>
2025-01-24 18:25:00 +0800

    Add files via upload

---
+++ 4K/sd.php
@@ -0,0 +1,34 @@
+[
+     {
+    "name": "本地包",
+    "list": [
+      {
+        "name": "多线路半本地包01-24",
+        "url": "http://123.yy.xn--dkw.xn--6qq986b3xl/tvbox.zip",
+        "icon": "http://127.0.0.1:9978/file/tvbox/.版本号.png",
+        "version": "当前版本防止域名被屏蔽用"
+      },
+      {
+        "name": "潇洒线上包1.22.1",
+        "url": " https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737688390644/单线路.zip",
+        "icon": "http://127.0.0.1:9978/file/TVBoxOSC/tvbox/.版本.png",
+        "version": "当前版本自行更新"
+      },
+      {
+    "name": "Allinone",
+    "list": [
+      {
+        "name": "线上包-32-1.23",
+        "url": "http://wb.xn--dkw.xn--6qq986b3xl:5244/d/%E5%A4%A9/allinone32.zip",
+        "icon": "http://127.0.0.1:9978/file/TVBox/.allinone.png",
+        "version": "当前版本胖羊专用"
+      },
+      {
+        "name": "线上包-64-1.23",
+        "url": "http://wb.xn--dkw.xn--6qq986b3xl:5244/d/%E5%A4%A9/allinone64.zip",
+        "icon": "http://127.0.0.1:9978/file/TVBox/.allinone.png",
+        "version": "当前版本胖羊专用"
+      }
+    ]
+  }
+]
